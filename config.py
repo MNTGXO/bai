dc8f9eb0c3e74fd90bea988717fd36008cc3924a
Musammil N <mohammedmusammiln@gmail.com>
2025-06-22 11:31:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,12 +13,5 @@ class API:
 class OWNER:
     ID = int(os.environ.get("OWNER", 0))
 
-class CHANNEL:
-    ID = int(os.environ.get("CHANNEL_ID", 0))
-
 class WEB:
     PORT = int(os.environ.get("PORT", 8000))
-
-class DATABASE:
-    URI = os.environ.get("DB_URI", "")
-    NAME = os.environ.get("DB_NAME", "MN_Bot_DB")
