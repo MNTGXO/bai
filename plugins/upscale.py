aa72c0b137ef897be5b1e77195195728cbfe859c
Musammil N <mohammedmusammiln@gmail.com>
2025-06-22 19:25:01 +0530

    Update upscale.py

--- plugins/upscale.py
+++ plugins/upscale.py
@@ -5,7 +5,7 @@ import os
 import ffmpeg
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
-from pyrogram.errors import MediaTooLarge
+from pyrogram.errors import FileTooLarge
 import logging
 
 logger = logging.getLogger(__name__)
